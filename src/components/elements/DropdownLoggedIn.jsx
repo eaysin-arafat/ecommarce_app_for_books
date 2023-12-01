1103098633dc57e3926897efb2423f776a668310
Eaysin <eaysin.arafat.j@gamil.com>
2023-12-01 20:26:42 +0600

    signgle product issue

--- src/components/elements/DropdownLoggedIn.jsx
+++ src/components/elements/DropdownLoggedIn.jsx
@@ -9,7 +9,6 @@ export const DropdownLoggedIn = ({ setDropdown }) => {
   const navigate = useNavigate();
   const [user, setUser] = useState({});
 
-  console.log(user);
   useEffect(() => {
     const fetchData = async () => {
       try {
